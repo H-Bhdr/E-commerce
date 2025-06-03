5300d7e450d89fc37582c6a9a9f25a40a11e5dc0
ziyayerden <ziyayerden@gmail.com>
2025-06-04 02:05:06 +0300

    Connectivity
    connectivity yapıldı

--- e_commerce_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ e_commerce_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -20,6 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
- 
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
